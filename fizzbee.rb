54e76d4864edfb9c268451bb5d1ffe3c8b005ee4
Li Yazhou <me.ssword@gmail.com>
2025-07-27 16:07:55 +0800

    revise fizzbee.rb

--- fizzbee.rb
+++ fizzbee.rb
@@ -1,14 +1,14 @@
 class Fizzbee < Formula
-  desc "FizzBee programming language interpreter"
+  desc "A formal specification language and model checker to specify distributed systems."
   homepage "https://github.com/fizzbee-io/fizzbee"
   version "0.1.2"
-  
+
   if Hardware::CPU.arm?
     url "https://github.com/fizzbee-io/fizzbee/releases/download/v0.1.2/fizzbee-v0.1.2-macos_arm.tar.gz"
-    sha256 "fbc2846e494f23bab6cb89ca89067a86551852a0c3cc2a796b51c11b1ed6614a2"
+    sha256 "bc2846e494f23bab6cb89ca89067a86551852a0c3cc2a796b51c11b1ed6614a2"
   else
     url "https://github.com/fizzbee-io/fizzbee/releases/download/v0.1.2/fizzbee-v0.1.2-macos_x86.tar.gz"
-    sha256 "REPLACE_WITH_ACTUAL_X86_SHA256"
+    sha256 "201378599098c7811d7a2fe59a5ca8a95430b8064871f254966e5e98d76f627b"
   end
 
   def install
@@ -17,7 +17,7 @@ class Fizzbee < Formula
     libexec.install "parser"
     libexec.install "fizz.env"
     libexec.install "fizz"
-    
+
     # Create wrapper script that sets correct environment variables
     (bin/"fizz").write <<~EOS
       #!/bin/bash
@@ -28,6 +28,19 @@ class Fizzbee < Formula
   end
 
   test do
-    system "#{bin}/fizz", "--help"
+    # Test basic execution with a simple FizzBee program
+    (testpath/"hello.fizz").write <<~EOS
+    action Init:
+      a = 0
+      b = 0
+
+    action Add:
+      oneof:
+        a = (a + 1) % 3
+        b = (b + 1) % 3
+    EOS
+
+    # Test that the interpreter can parse and potentially execute a basic program
+    system "#{bin}/fizz", "hello.fizz"
   end
 end
