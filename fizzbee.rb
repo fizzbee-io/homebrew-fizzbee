0b3be65d61166d7508e34fe886de16ed7820e264
Li Yazhou <me.ssword@gmail.com>
2025-07-27 15:22:49 +0800

    use a wrapper script

--- fizzbee.rb
+++ fizzbee.rb
@@ -1,34 +1,29 @@
 class Fizzbee < Formula
   desc "FizzBee programming language interpreter"
-  homepage "https://github.com/fizzbee-io/fizzbee"  # 替换为你的实际仓库
-  url "https://github.com/fizzbee-io/fizzbee/releases/download/v0.1.2/fizzbee-v0.1.2-macos_arm.tar.gz"  # 替换为实际下载链接
-  sha256 "fbc2846e494f23bab6cb89ca89067a86551852a0c3cc2a796b51c11b1ed6614a2"  # 需要计算实际的SHA256
+  homepage "https://github.com/fizzbee-io/fizzbee"
   version "0.1.2"
-
-  depends_on "python@3.12"
+  
+  if Hardware::CPU.arm?
+    url "https://github.com/fizzbee-io/fizzbee/releases/download/v0.1.2/fizzbee-v0.1.2-macos_arm.tar.gz"
+    sha256 "fbc2846e494f23bab6cb89ca89067a86551852a0c3cc2a796b51c11b1ed6614a2"
+  else
+    url "https://github.com/fizzbee-io/fizzbee/releases/download/v0.1.2/fizzbee-v0.1.2-macos_x86.tar.gz"
+    sha256 "REPLACE_WITH_ACTUAL_X86_SHA256"
+  end
 
   def install
-    bin.install "fizzbee"
-    
+    # Install all files to libexec
+    libexec.install "fizzbee"
     libexec.install "parser"
-    
     libexec.install "fizz.env"
+    libexec.install "fizz"
     
-    (bin/"fizzbee").write <<~EOS
+    # Create wrapper script that sets correct environment variables
+    (bin/"fizz").write <<~EOS
       #!/bin/bash
-      export SCRIPT_DIR="#{libexec}"
-      source "#{libexec}/fizz.env"
-      exec "#{libexec}/fizzbee" "$@"
-    EOS
-    
-    rm "#{bin}/fizzbee"
-    mv "#{libexec}/fizzbee", "#{bin}/fizzbee-bin"
-    
-    (bin/"fizzbee").write <<~EOS
-      #!/bin/bash
-      export SCRIPT_DIR="#{libexec}"
-      source "#{libexec}/fizz.env"
-      exec "#{bin}/fizzbee-bin" "$@"
+      export PARSER_BIN="#{libexec}/parser/parser_bin"
+      export FIZZBEE_BIN="#{libexec}/fizzbee"
+      exec "#{libexec}/fizz" "$@"
     EOS
   end
 
